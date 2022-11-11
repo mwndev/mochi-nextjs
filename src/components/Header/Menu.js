cb072fc485d497fe2fbee20eb7bc461c64239816
politicallyCorrectName <martin.wiedermann1@gmail.com>
2022-11-11 15:15:04 +0100

    first commit

---
+++ src/components/Header/Menu.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const OuterBox = styled.nav`
+    height: 100vh;
+    position: fixed;
+    max-width: 90vw;
+    width: calc(5cm + 18vw);
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: baseline;
+`
+
+export default function Menu() {
+  return (
+    
+  )
+}
