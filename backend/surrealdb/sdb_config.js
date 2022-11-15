acf62ad0967ac8eda214717e3ab939437cfb8618
politicallyCorrectName <martin.wiedermann1@gmail.com>
2022-11-15 20:35:06 +0100

    made successful surrealdb query

--- backend/surrealdb/sdb_config.js
+++ backend/surrealdb/sdb_config.js
@@ -12,45 +12,46 @@ async function main() {
     });
 
     // Select a specific namespace / database
-    await db.use("test", "test");
+    await db.use("kusa", "kusa");
 
     // Create a new person with a random id
-    let created = await db.create("person", {
-      title: "Founder & CEO",
-      name: {
-        first: "Tobie",
-        last: "Morgan Hitchcock",
+    let created = await db.create("location", {
+      email: "locationatmarszalkowska@gmail.com",
+      phone: "495 929 292 11",
+      address: {
+        street: "marszalkowska 8 / 18",
+        zip: "01-144",
+        city: "warsaw",
+        region: "masowieckie",
+        country: "poland",
       },
-      marketing: true,
-      identifier: Math.random().toString(36).substring(2, 10),
     });
-    console.log("created");
+    console.log("created:");
     console.log(created);
 
     // Update a person record with a specific id
-    let updated = await db.change("person:jaime", {
-      marketing: true,
-    });
-    console.log("updated");
-    console.log(updated);
+    // let updated = await db.change("location", {
+    // marketing: false,
+    // });
+    // console.log("updated");
+    // console.log(updated);
 
     // Select all people records
-    let people = await db.select("person");
-    console.log("people");
-    console.log(people);
+    // let people = await db.select("person");
+    // console.log("people");
+    // console.log(people);
 
     // Perform a custom advanced query
-    // let groups = await db.query(
-    //   "SELECT marketing, count() FROM type::table($tb) GROUP BY marketing",
-    //   {
-    //     tb: "person",
-    //   }
-    // );
-    // console.log("groups");
-    // console.log(groups);
+    //   let groups = await db.query(
+    //     "SELECT marketing, count() FROM type::table($tb) GROUP BY marketing",
+    //     {
+    //       tb: "person",
+    //     }
+    //   );
+    //   console.log("groups");
+    //   console.log(groups);
   } catch (e) {
     console.error("ERROR", e);
   }
 }
-
 main();
